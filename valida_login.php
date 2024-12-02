92e4ddb576ffcf1c2f72b15cb61174bbfdf5c43c
INGRIDBATSISTAMS <ingridmarianb92@gmail.com>
2024-12-02 17:08:00 -0300

    alterações finais

--- valida_login.php
+++ valida_login.php
@@ -1,44 +1,65 @@
 <?php
 
-    session_start();
-
-    //variavel que verifica se a autenticação foi realizada
-    $usuario_autenticado = false;
+session_start();
 
 
-    //usuarios do sistema
-    $usuarios_app = array(
-        array('email' => 'adm@teste.com.br', 'senha' => '123456'),
-        array('email' => 'user@teste.com.br', 'senha' => 'abcd'),
-    );
+//VARIAVEL QUE VERIFICA SE A AUTENTICAÇÃO FOI REALIZADA
+$usuario_autenticado = false;
+$usuario_id = null;
+$usuario_perfil_id = null;
 
-    // echo '<prev';
-    // print_r ($usuarios_app);
-    // echo '</prev>';
+$perfis = array(1 => 'Administrativo', 2 => 'Usuário');
 
-    foreach($usuarios_app as $user){
+//USUARIOS DO SISTEMA
+$usuarios_app = array(
+    array('id' => 1, 'email' => 'adm@teste.com.br', 'senha' => '1234', 'perfil_id' => 1),
+    array('id' => 2, 'email' => 'user@teste.com.br', 'senha' => '1234','perfil_id' => 1),
+    array('id' => 3, 'email' => 'jose@teste.com.br', 'senha' => '1234','perfil_id' => 2),
+    array('id' => 4, 'email' => 'maria@teste.com.br', 'senha' => '1234','perfil_id' => 2)
+);
+/*
 
-        if($user['email'] ==  $_POST['email'] && $user['senha'] ==  $_POST['senha']){
-            $usuario_autenticado = true;
-        }
-   
+echo '<pre>';
+print_r($usuarios_app);
+echo '</pre>';
+
+*/
+
+foreach($usuarios_app as $user){
+    /*
+    echo 'Usuario app: ' . $user['email'] . '/' . $user['senha'];
+    echo '<br />';
+    echo 'Usuario form: ' . $_POST['email'] . '/' . $_POST['senha'];
+    echo '<hr />';
+    */
+    if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']){
+        $usuario_autenticado = true;
+        $usuario_id = $user['id'];
+        $usuario_perfil_id = $user['perfil_id'];
     }
+}
 
-    if ($usuario_autenticado){
-        echo 'Usuário autenticado';
+    if($usuario_autenticado){
+        echo 'Usuário autenticado.';
+
         $_SESSION['autenticado'] = 'SIM';
-        header('Location: home.php);
-    }else{
-        $_SESSION['autenticado'] = 'NAO';
+        $_SESSION['id'] = $usuario_id;
+        $_SESSION['perfil_id'] = $usuario_perfil_id;
+        header('Location: home.php');
+    }else{  
+        $_SESSION['autenticado'] = 'NÃO';
         header('Location: index.php?login=erro');
-    }
+      }
 
-    // $_GET['email'];
-    // $_GET['senha'];
 
-    // print_r($_POST);
-    // echo '<br/>';
-    // echo $_POST['email'];
-    // echo '<br/>';
-    // echo $_POST['senha'];
+/*
+print_r($_GET)
+
+echo '<br />'
+echo $_GET['email']
+echo '<br />'
+echo $_GET['senha']
+print_r($_POST);
+*/
+
 ?>
